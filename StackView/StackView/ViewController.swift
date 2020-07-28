40ed8321b480f52e4b538b816498ce7c199ed04b
igor.lyubin <ilyubin@ozon.ru>
2020-07-29 00:13:52 +0300

    StackView

---
+++ StackView/StackView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Lyubin Igor on 28.07.2020.
+//  Copyright © 2020 AP Organization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
