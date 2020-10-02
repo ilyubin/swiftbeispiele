13503f0ef5d741d93fd73b80e76fb89b5753705b
igor.lyubin <ilyubin@ozon.ru>
2020-10-02 22:31:13 +0300

    spotifyClone init

---
+++ SpotifyClone/SpotifyClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by Lyubin Igor on 02.10.2020.
+//  Copyright © 2020 AP Organization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
